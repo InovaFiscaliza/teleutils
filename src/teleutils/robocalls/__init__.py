036ca3b87a01b0f0156f1d9f12908fdfacc5d398
Maxwel de Souza Freitas <maxwel@maxwelfreitas.com.br>
2026-03-16 13:08:56 -0300

    feat(robocalls): ✨ Add RoboCallsAnalyzer for analyzing call data

--- src/teleutils/robocalls/__init__.py
+++ src/teleutils/robocalls/__init__.py
@@ -1,7 +1,8 @@
 # O NullHandler já é registrado ao importar _logging.
 # Aqui apenas garantimos que isso acontece ao carregar o pacote.
 import teleutils._logging  # noqa: F401
+from teleutils.robocalls._analyzers import RoboCallsAnalyzer
 from teleutils.robocalls._extractors import RoboCallsExtractor
 from teleutils.robocalls._transformers import RoboCallsTransformer
 
-__all__ = ["RoboCallsExtractor", "RoboCallsTransformer"]
+__all__ = ["RoboCallsExtractor", "RoboCallsTransformer", "RoboCallsAnalyzer"]
---
+++ src/teleutils/robocalls/_analyzers.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+import logging
+
+from pyspark.sql import DataFrame, SparkSession
+from pyspark.sql import functions as F
+
+logger = logging.getLogger(__name__)
+
+
+class RoboCallsAnalyzer:
+    def __init__(self, spark: SparkSession):
+        self.spark = spark
+
+    def analyze(self, source_file: str, target_file: str = "") -> DataFrame:
+        df = self.spark.read.parquet(source_file)
+        df_agg = (
+            df.groupBy("numero_de_a_formatado", "hora_da_chamada")
+            .agg(
+                F.count("referencia").alias("total_chamadas"),
+                F.sum("chamada_curta").alias("total_chamadas_curtas"),
+                F.sum("chamada_caixa_postal").alias("total_chamadas_caixa_postal"),
+                F.sum(
+                    F.when(
+                        F.col("chamada_autenticada") > 0, F.col("chamada_autenticada")
+                    ).otherwise(0)
+                ).alias("total_chamadas_autenticadas"),
+            )
+            .orderBy(F.desc("total_chamadas_curtas"))
+        )
+        if target_file:
+            df_agg.write.mode("overwrite").parquet(target_file)
+            df_agg = self.spark.read.parquet(target_file)
+        return df_agg
