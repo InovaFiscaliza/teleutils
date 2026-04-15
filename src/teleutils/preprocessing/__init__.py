7f587ff24a7d7b286dfa46391d0b234aea1a9f6d
Maxwel de Souza Freitas <maxwel@maxwelfreitas.com.br>
2026-04-15 13:22:23 -0300

    feat(preprocessing): ✨ Add spark_normalize_number to exports

--- src/teleutils/preprocessing/__init__.py
+++ src/teleutils/preprocessing/__init__.py
@@ -1,6 +1,7 @@
 from teleutils.preprocessing.number_format import (
     normalize_number,
     normalize_number_pair,
+    spark_normalize_number,
 )
 
-__all__ = ["normalize_number", "normalize_number_pair"]
+__all__ = ["normalize_number", "normalize_number_pair", "spark_normalize_number"]
