ad1e741901a75265dd07a5a7b08ca7aa1a88bea0
Maxwel de Souza Freitas <maxwel@maxwelfreitas.com.br>
2026-06-11 20:25:13 -0300

    feat(extractors): ✨ Add CDRTeleparserExtractor for Ericsson CDRs - Introduce CDRTeleparserExtractor class for extracting Ericsson CDRs - Implement CDRTeleparserSchema for schema validation - Update __init__.py to include new extractor

--- src/teleutils/core/extractors/__init__.py
+++ src/teleutils/core/extractors/__init__.py
@@ -1,4 +1,4 @@
-from teleutils.core.extractors._text import CDRTextExtractor
-from teleutils.core.extractors._teleparser import CDRTeleparserExtractor
+from teleutils.core.extractors._text_extractors import CDRTextExtractor
+from teleutils.core.extractors._teleparser_extractors import CDRTeleparserExtractor
 
 __all__ = ["CDRTextExtractor", "CDRTeleparserExtractor"]
