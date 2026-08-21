bbcb124480cc84078de973d2e08a2ec0dd2e25d3
Maxwel de Souza Freitas <maxwel@maxwelfreitas.com.br>
2026-08-21 12:14:26 -0300

    fix(transformers): 🐛 Correct regex extraction for cell ID

--- src/teleutils/core/transformers/teleparser_transformers.py
+++ src/teleutils/core/transformers/teleparser_transformers.py
@@ -339,7 +339,7 @@ class CDRTeleparserTransformer(CDRBaseTransformer):
         df = df.withColumn(
             "_cell_id",
             F.regexp_extract(
-                "_informacao_rede", r"utran-cell-id-3gpp=([0-9a-zA-Z]+);", 1
+                F.col("_informacao_rede"), r"utran-cell-id-3gpp=([0-9a-zA-Z]+);", 1
             ),
         )
         df = _format_cell_id(df, "_cell_id", "_cell_id")
