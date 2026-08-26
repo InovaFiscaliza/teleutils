9d1ab36381d93a905efabe4ed2d53a330e854da4
Maxwel de Souza Freitas <maxwel@maxwelfreitas.com.br>
2026-08-26 17:05:19 -0300

    fix(config): 🐛 Update MIN_SAFE_DATE to ensure proper date handling

--- src/teleutils/_config.py
+++ src/teleutils/_config.py
@@ -49,7 +49,7 @@ MAX_RECORDS_PER_FILE = 1000000
 SHORT_CALL_THRESHOLD = 6
 
 # Define a data limite como um literal do Spark para o Catalyst otimizar a comparação
-MIN_SAFE_DATE = F.lit("1900-01-01 00:00:00").cast(TimestampType())
+MIN_SAFE_DATE = F.lit("1901-01-01 00:00:00").cast(TimestampType())
 
 # Código MCC/MNC para preenchimento em caso de ausência de informação de operadora, utilizado em transformações
 # de CDRs para manter consistência de dados e evitar valores nulos em campos críticos
